f3d9c90b11db5e6a6c27ff0116dcaf5638a5af2f
yogesh0224 <yogeshluitel9@gmail.com>
2024-10-29 20:58:32 +0545

    static files loaded

--- khopnepal/settings.py
+++ khopnepal/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
